f74595fd180306a9d68b84e6b17d05ca0d2a66f7
Bright Lazarus <lazbright1@gmail.com>
2023-02-27 00:23:24 +0100

    Implented the reviews

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,23 +3,24 @@ const message = document.querySelector("#block");
 
 const locked = document.querySelectorAll(".locked");
 const lock = document.querySelector("#lock");
+const backButton = document.querySelector("#back");
 locked.forEach((item) => {
   item.addEventListener("click", () => {
-    console.log("why");
     lock.style.display = "flex";
-    //   lock.classList.remove("fade-out");
+    lock.classList.remove("fade-out");
     setTimeout(() => {
       lock.classList.add("fade-in");
     }, 300);
   });
 });
 
-// To be used when a button is created to close the lock page
-// shadow.classList.add("fade-out");
-// setTimeout(() => {
-//   shadow.classList.remove("fade-in");
-//   shadow.style.display = "none";
-// }, 300);
+backButton.addEventListener("click", () => {
+  lock.classList.add("fade-out");
+  setTimeout(() => {
+    lock.classList.remove("fade-in");
+    lock.style.display = "none";
+  }, 300);
+});
 
 const checkPassword = () => {
   let password = document.getElementById("password").value;
